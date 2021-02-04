78b472e7215c2c641e41f0c6aa771947bc1afe39
Brian Forbes <brianandrewf81@gmail.com>
2021-02-03 18:13:20 -0700

    Create Movie component

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import movieData from '../test/test-data';
+import Movie from './Movie'
 
 class MainDisplay extends React.Component {
   constructor() {
     super()
     this.state = {
-      movies: movieData;
+      movies: movieData
     };
   }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Movie = (props) => {
+  return (
+  <img src={props.poster}/>
+  )
+}
+
+
+
+export default Movie;
