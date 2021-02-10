8016fa7be2fc3bb2eb81b6d15806df24a4f6ae6d
Brian Forbes <brianandrewf81@gmail.com>
2021-02-09 19:14:37 -0700

    Add conditional rendering for error message 404 not found

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+import React from 'react'
 import './App.css';
 import MainDisplay from './components/MainDisplay';
-import {Route, Switch} from "react-router-dom"
+import {Route, Switch, Redirect} from "react-router-dom";
 import IndividualView from './components/IndividualView';
+import Error from './components/Error';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainDisplay}/>
-        <Route path="/movie/:id" 
-        render={(props) => <IndividualView {...props} />}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      errorMessage: "",
+    };
+  }
+
+  componentDidMount = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+    .then(data => this.setState({
+      movies: data.movies,
+      errorMessage: ""
+    }))
+  .catch(error => {
+        console.log(error.message)
+        this.setState({errorMessage: error})
+        console.log(this.state.errorMessage)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+        <Route path="/" exact render={() => this.state.errorMessage ? <Error /> : <MainDisplay movies={this.state.movies}/>}/>
+          <Route path="/movie/:id"
+          render={(props) => <IndividualView {...props} />}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
