1b24ca71bc3177148a2323b4ae74ed5ad5d99903
Drew Bradley <williamandrewb@gmail.com>
2021-02-10 19:35:01 -0700

    Test images in Footer.

--- cypress/integration/main_display_spec.js
+++ cypress/integration/main_display_spec.js
@@ -6,17 +6,22 @@ describe('App Elements', () => {
       cy.get('div').should("have.class", "background-img")
       
     });
-    it('Should render 40 movie thumbnails', () => {
+  it('Should render 40 movie thumbnails', () => {
       cy.get('section').find('a').should("have.length", 40)
       cy.get('section').contains('a').click({multiple: true})
     });
   it('Should be able to navigate to individual movie and render elements', () => {
-    cy.intercept('movies/581392', { fixture: 'example.json' }).as('getMovie')
-    // cy.visit('http://localhost:3000/movie/581392')
-    cy.request('https://rancid-tomatillos.herokuapp.com/api/v2/movies/581392')
-    cy.wait('@getMovie')
-      cy.get('footer').contains('div')
+    cy.visit('http://localhost:3000/movie/581392')
+      cy.get('footer', { timeout: 10000 }).should('be.visible')
+      cy.get('footer').find('img').should('have.attr', 'src').should('include', "https://image.tmdb.org/t/p/original//sy6DvAu72kjoseZEjocnm2ZZ09i.jpg")
+      cy.get('footer').find('div')
       cy.get('button').contains('GO BACK')
       // Check button clickable after style change
+    cy.visit('http://localhost:3000/movie/337401')
+      cy.get('footer', { timeout: 10000 }).should('be.visible')
+      cy.get('footer').find('img').should('have.attr', 'src').should('include', "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
+    cy.visit('http://localhost:3000/movie/694919')
+      cy.get('footer', { timeout: 10000 }).should('be.visible')
+      cy.get('footer').find('img').should('have.attr', 'src').should('include', "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
   });
 });
