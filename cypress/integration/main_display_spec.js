01f8be15a03112c01702e135e21594126bcae953
Drew Bradley <williamandrewb@gmail.com>
2021-02-10 17:22:51 -0700

    Write first five tests for main display.

--- cypress/integration/main_display_spec.js
+++ cypress/integration/main_display_spec.js
@@ -1,5 +1,9 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+describe('App Elements', () => {
+  it('Should be able to visit the page and render the correct elements', () => {
+    cy.visit('http://localhost:3000')
+      cy.get('div').should("have.id", "root")
+      cy.get('section').contains('a')
+      cy.get('div').should("have.class", "background-img")
+      cy.get('section').find('a').should("have.length", 40)
+  });
+});
