7052ef8003d50a492a72901e9f4937c65a9f9b49
Brian Forbes <brianandrewf81@gmail.com>
2021-02-13 10:32:43 -0700

    Add aria labels and h1 element to thumbnails to achieve accessibility score of 100

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom"
 
 const Movie = (props) => {
   return (
-    <Link to={`/movie/${props.id}`} id={props.id}>
+    <Link to={`/movie/${props.id}`} id={props.id} aria-label={props.title}>
       <div className="background-img"
       style={{
         backgroundImage: `url(${props.poster})`, backgroundRepeat: "no-repeat",
         backgroundSize: "cover"}}
       >
+        <h1 className="hidden-title">{props.title}</h1>
         <div className="title-container">
         </div>
       </div>
