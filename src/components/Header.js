67fd4f15d46040e941c8221326e19e63ee59813b
Brian Forbes <brianandrewf81@gmail.com>
2021-02-13 11:37:37 -0700

    Add custom Mr. Potato logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import grouch from '../grouch-potato.png'
 
 const Header = () => {
   return (
     <header>
-      <h1 className="app-name">Grouch Potato</h1>
+      <div className='logo-and-name-wrapper'>
+        <img className='logo' src={grouch}/>
+        <h1 className="app-name">Grouch Potato</h1>
+      </div>
       <div className="input-wrapper">
         <label for="search"></label>
         <input placeholder="Search Movies By Title" type="text" name="search" className="search-bar"></input>
