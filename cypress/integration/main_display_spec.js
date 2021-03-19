217b463dfa30a8d8b0f25c0ea66b205770e6c177
Drew Bradley <williamandrewb@gmail.com>
2021-03-19 12:26:50 -0600

    Rewrite stubs.

--- cypress/integration/main_display_spec.js
+++ cypress/integration/main_display_spec.js
@@ -1,20 +1,27 @@
-describe('App Elements', () => {
-  it('Should be able to visit the page and render the correct elements', () => {
-    cy.visit('http://localhost:3000')
-      cy.get('div').should("have.id", "root")
-      cy.get('section').find('a')
-      cy.get('div').should("have.class", "background-img")
-    });
-
-  it('Should render 40 movie thumbnails upon page load', () => {
-      cy.get('section').find('a').should("have.length", 40)
-    });
-
-  it('Should fetch shallow dataset for all movies upon page load', () => {
+context('Main Display', () => {
+  beforeEach(() => {
     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {fixture: 'movies.json'}).as('movieRoute')
     cy.visit('http://localhost:3000/')
-    cy.wait('@movieRoute').then((interception) => {
-      'response.ok'
+  })
+
+  describe('App Elements', () => {
+    it('Should be able to visit the page and render the correct elements', () => {
+      cy.visit('http://localhost:3000/')
+        cy.get('div').should("have.id", "root")
+        cy.get('section').find('a')
+        cy.get('div').should("have.class", "background-img")
+      });
+
+    it('Should render 40 movie thumbnails upon page load', () => {
+        cy.get('section').find('a').should("have.length", 40)
+      });
+
+    it('Should fetch shallow dataset for all movies upon page load', () => {
+      cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {fixture: 'movies.json'}).as('movieRoute')
+      cy.visit('http://localhost:3000/')
+      cy.wait('@movieRoute').then((interception) => {
+        'response.ok'
+      })
     })
   })
-});
+})
